4841862d9457661a730cbaea379cdacbeb6ae5b5
faysalhosen <faysanhosen7025@gmail.com>
2023-10-27 17:03:04 +0600

    add banner section with responsive

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,10 @@
+import Banner from "../../components/Banner/Banner";
 import Navbar from "../../components/Navbar/Navbar";
 const Home = () => {
   return (
     <div>
       <Navbar/>
+      <Banner></Banner>
       
      
     </div>
