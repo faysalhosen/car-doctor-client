ef38644266241e3187d99883c25d0c638a408af1
faysalhosen <faysanhosen7025@gmail.com>
2023-10-28 02:07:15 +0600

    service section data loaded

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import AboutUs from "../../components/AboutUs/AboutUs";
 import Banner from "../../components/Banner/Banner";
 import Navbar from "../../components/Navbar/Navbar";
+import Services from "../../components/Services/Services";
 const Home = () => {
   return (
     <div>
       <Navbar/>
       <Banner/>
       <AboutUs/>
+      <Services/>
      
     </div>
   );
---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,29 @@
+import { useQuery } from "@tanstack/react-query";
+import ServiceCard from "./ServiceCard";
+const Services = () => {
+    const {isPending, data} = useQuery({
+        queryKey: ["servies"],
+        queryFn: async()=>{
+           const res = await fetch("services.json")
+           return res.json()
+        }
+    })
+    if(isPending){
+        return <div>
+            <span className="loading loading-bars loading-lg"></span>
+        </div>
+    }
+
+
+  return (
+      <div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+            {
+                data.map(service=> <ServiceCard key={service._id} service={service}/>)
+            }
+        </div>
+      </div>
+      )
+};
+
+export default Services;
